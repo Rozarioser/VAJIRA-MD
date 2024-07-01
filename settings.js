0a4f0131ddd9aefe19bc7342502cfb136c012d2d
Rozarioser <153474955+Rozarioser@users.noreply.github.com>
2024-07-02 00:55:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,16 +6,16 @@ function convertToBool(text, fault = 'true') {
 
 
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
-const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
-const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+const BOTNAME = 'ᏒᎾᏃᎪᏒᎥᎾွဳ⃟ํํํํํํ⃝𝄞sᎬᏒ|-WA-BOT™';
+const FOOTERNAME = '> 🦋⃟•𝆺𝚪𝚯𝚭𝚫𝚪𝚰𝚯 𝐒𝚵𝚪☣☠︎︎ - Ｖ4';
+global.owner = process.env.OWNER_NUMBER  || '916282524515' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=jElxDKKB#uKHthvUO6fuTbjuZzq4SC0GvyVbDJjqJDeLgJNXBBuw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "916282524515,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+OWNER_NAME: process.env.OWNER_NAME || '🦋⃟•𝆺𝚪𝚯𝚭𝚫𝚪𝚰𝚯 𝐒𝚵𝚪☣☠︎︎' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
